34896fd2abff336cf16dbe6f2bc4ded924761c09
DevTruth <devtruth17@gmail.com>
2022-06-10 20:06:44 +0100

    still fixing more bugs on CSP

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "img-src": ["'self'", "https: data:"],
+      "img-src": ["'self'", "https: data: blob:"],
       "script-src": ["'self'", "https: data:"],
-      "connect-src": ["'self'", "https: data: blob:"]
+      "connect-src": ["'self'", "https: data:"]
     }
   })
 )
